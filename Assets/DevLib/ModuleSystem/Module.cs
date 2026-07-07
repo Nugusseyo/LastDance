800ac4cc814ebbce37b27ab9c598999af7165069
ForRest1217 <Jihan2519@naver.com>
2026-07-07 19:21:56 +0900

    IModule

--- Assets/DevLib/ModuleSystem/Module.cs
+++ Assets/DevLib/ModuleSystem/Module.cs
@@ -2,7 +2,7 @@
 
 namespace DevLib.ModuleSystem
 {
-    public abstract class Module : MonoBehaviour
+    public abstract class Module : MonoBehaviour, IModule
     {
         protected ModuleOwner _owner;
         public virtual void Initialize(ModuleOwner owner)
---
+++ Assets/_Works/JJH/02_Scripts/Agents/AgentSensor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace JJH._02_Scripts.Agents
+{
+    public class AgentSensor : MonoBehaviour
+    {
+
+    }
+}
