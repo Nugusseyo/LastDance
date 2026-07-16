ba8633e3a93b60ef5ac0d05bc5f76521c8420191
ForRest1217 <Jihan2519@naver.com>
2026-07-16 19:45:21 +0900

    AgentSensorFixed

--- Assets/_Works/JJH/02_Scripts/Agents/ISensor.cs
+++ Assets/_Works/JJH/02_Scripts/Agents/ISensor.cs
@@ -4,8 +4,10 @@ namespace JJH._02_Scripts.Agents
 {
     public interface ISensor
     {
-        public bool IsTargetInSight(Transform targetTrm, float viewAngle);   //시야각 안에 있는가(타겟)
-        public bool IsTargetInSight(float viewAngle, float checkDistance, out Collider collid);   //시야각 안에 있는가(타겟X)
+        Collider[] ColliderResults { get; }
+
+        bool IsTargetInSight(Transform targetTrm, float viewAngle);   //시야각 안에 있는가(타겟)
+        bool IsTargetInSight(float viewAngle, float checkDistance);   //시야각 안에 있는가(타겟X)
         bool IsTargetInViewRadius(float range, out Collider hitCollider); //사거리 안에 있는가(원형 감지)
         int FindTargetsInRadius(float viewRadius); //사거리 안에 얼마나 있는가(원형 감지)
     }
