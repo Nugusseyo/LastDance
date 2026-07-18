c44a8bc20e424c8a01b7d0cef45cb377fd10541d
ForRest1217 <Jihan2519@naver.com>
2026-07-18 21:35:54 +0900

    LaughingDevil

--- Assets/_Works/JJH/02_Scripts/Agents/Enemies/INavMeshAgent.cs
+++ Assets/_Works/JJH/02_Scripts/Agents/Enemies/INavMeshAgent.cs
@@ -4,7 +4,8 @@ namespace JJH._02_Scripts.Agents.Enemies
 {
     public interface INavMeshAgent
     {
-        void MoveTo(Vector3 targetPosition);
+        void MoveTo(Vector3 targetPos);
+        public void MoveToFixedPos(Vector3 targetPos, Vector3 fixedPos);
         void StopImmediately();
     }
 }
