b5eeca785f7788ad2e27f73e4f48cdbaa33ce386
Nugusaeyo <chsj0702@gmail.com>
2026-09-02 17:35:21 +0900

    UpgradeDB

---
+++ Assets/Resources/DataBase/Upgrade Data/UpgradeDB.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Resources.DataBase.Upgrade_Data
+{
+	[ExcelAsset]
+	public class UpgradeDB : ScriptableObject
+	{
+		//public List<EntityType> UpgradeSheet; // Replace 'EntityType' to an actual type that is serializable.
+	}
+}
