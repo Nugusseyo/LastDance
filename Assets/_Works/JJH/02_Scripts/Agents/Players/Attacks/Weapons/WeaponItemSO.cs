8291da837777c111a23a9fb905db92ec46f3777c
ForRest1217 <Jihan2519@naver.com>
2026-09-09 20:20:03 +0900

    ItemDataSO

---
+++ Assets/_Works/JJH/02_Scripts/Agents/Players/Attacks/Weapons/WeaponItemSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Works.JJH._02_Scripts.Agents.Players.Attacks.Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon Data", menuName = "SO/Weapon")]
+    public class WeaponItemSO : ItemDataSO
+    {
+        [field: SerializeField] public int Damage { get; private set; }
+        [field: SerializeField] public float AttackCooltime { get; private set; }
+    }
+}
