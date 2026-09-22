8763fcba6ca9636e38f11094a972935e1279320b
Nugusaeyo <chsj0702@gmail.com>
2026-09-22 10:11:12 +0900

    Input fix

---
+++ Assets/_Works/JYG/_Scripts/Input/UIInputSO.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "UIInputSO", menuName = "Scriptable Objects/UIInputSO")]
+public class UIInputSO : ScriptableObject, Controls.IUIActions
+{
+    public event Action OnEscapePressed;
+
+    private Controls _controls;
+    private bool _createdInput = false;
+
+    private void OnEnable()
+    {
+        if(_controls == null)
+        {
+            _controls = new Controls();
+            _createdInput = true;
+        }
+        
+        SetUpInput();
+        _controls.UI.Disable();
+    }
+
+    private void OnDisable()
+    {
+        if (_controls != null)
+        {
+            CleanUpInput();
+        }
+    }
+
+    private void CleanUpInput()
+    {
+        _controls.UI.RemoveCallbacks(this);
+        _controls.UI.Disable();
+
+        if (_createdInput)
+        {
+            _controls.Dispose();    //만들어진 Controls는 Dispose로 제거해줘야 함.
+            _createdInput = false;
+        }
+        
+        _controls = null;
+    }
+
+    public void InitializeInput(Controls controls)
+    {
+        if (_controls != null)
+            CleanUpInput();
+        
+        _controls = controls;   // ??구문으로 아래 if 없앨 수 있지만, 가독성이 안좋다고 판단함.
+
+        if (_controls == null)
+        {
+            _controls = new Controls();
+            _createdInput = true;
+        }
+
+        SetUpInput();
+    }
+
+    private void SetUpInput()
+    {
+        _controls.UI.SetCallbacks(this);
+    }
+
+    public void SetEnable(bool enable)
+    {
+        if(enable)
+            _controls.UI.Enable();
+        else
+            _controls.UI.Disable();
+    }
+
+    public void OnEscape(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnEscapePressed?.Invoke();
+    }
+}
--- Assets/_Works/JYG/_Scripts/UI/Keyboard Viewer/KeyboardViewer.cs
+++ Assets/_Works/JYG/_Scripts/UI/Keyboard Viewer/KeyboardViewer.cs
@@ -1,16 +1,38 @@
+using System;
+using _Works.JJH._02_Scripts.Agents.Players;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-public class KeyboardViewer : MonoBehaviour
+namespace _Works.JYG._Scripts.UI.Keyboard_Viewer
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class KeyboardViewer : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Key targetKey = Key.E; //Key는 New InputSystem에서, 키보드의 키들을 넣어놓은 enum이다.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private UIInputSO uiInput;
+        [SerializeField] private PlayerInputSO playerInput;
+
+        public UnityEvent OnKeyPressed;
+
+        private void Update()
+        {
+            if (Keyboard.current[targetKey].wasPressedThisFrame)
+            {
+                uiInput.SetEnable(true);
+                playerInput.SetEnable(false);
+                OnKeyPressed?.Invoke();
+
+                uiInput.OnEscapePressed += HandleEscapeKeyPressed;
+            }
+        }
+
+        private void HandleEscapeKeyPressed()
+        {
+            uiInput.SetEnable(false);
+            playerInput.SetEnable(true);
+            
+            uiInput.OnEscapePressed -= HandleEscapeKeyPressed;
+        }
     }
 }
